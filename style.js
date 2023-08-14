3d8e84e295ee0cb530ed3bd995e254a5b588bc90
generator <generatortheforce@gmail.com>
2023-08-14 14:21:19 +0200

    weather

---
+++ style.js
@@ -0,0 +1,19 @@
+//let waether = {
+//apikey: "a88803f5d2bdb2de1ac9ab242f6c1221",
+//fetchWeather: function () {
+//fetch(
+//"https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metricappid=bd5a47ab866f34f8203fdbe4bfe806ff",
+//)
+//.then((Response) => Response.json())
+//.then((data) => this.displayWeather(data));
+//},
+//displayWeather: function (data) {
+//const { name } = data;
+//const { icon, discription } = data.waether;
+// const { temp, humidity } = data.main;
+//const { speed } = data.wind;
+//console.log(name, icon, discription, temp, humidity, speed);
+//document.querySelector(".city").innerText = " Weather in" + name;
+//document.querySelector(".icon").src = "./";
+//},
+//};
